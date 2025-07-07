c9ea2268b15cc15434eb10a4de07c59d493356ca
Shant Dashjian <shantgdashjian@gmail.com>
2025-07-07 15:36:14 +0400

    Add splitting

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main():
             for shot in shots:
                 if shot.collides_with(asteroid):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
                     break
 
         for asteroid in asteroids:
